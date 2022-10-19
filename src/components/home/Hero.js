ce90a9b609f475bbc4730ba5d9a697a8c074004b
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2022-10-19 20:21:38 +0530

    updates

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button } from "@mui/material";
 import GetQuote from "./GetQuote";
+import { useNavigate } from "react-router-dom";
 function Hero() {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="hero-container">
@@ -9,7 +11,12 @@ function Hero() {
           <span className="hero-title-1">Makineni</span> Projects
         </p>
         <p className="hero-desc">We make your dreams a reality</p>
-        <Button variant="contained" className="hero-button">
+
+        <Button
+          variant="contained"
+          className="hero-button"
+          onClick={() => navigate("/works")}
+        >
           Our Work
         </Button>
         <GetQuote />
