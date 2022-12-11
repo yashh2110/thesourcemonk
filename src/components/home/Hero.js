f7f7ad2d5c1c81ec1a28ba3a9d19a834f0979750
yashh2110 <yashwanth.muddana@gmail.com>
2022-12-11 18:10:35 +0530

    updates

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Button } from "@mui/material";
 import GetQuote from "./GetQuote";
 import { useNavigate } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
 function Hero() {
   const navigate = useNavigate();
   return (
@@ -11,13 +13,11 @@ function Hero() {
           The Sourcemonk<span className="hero-title-1">.com</span>
         </p>
         <p className="hero-desc">A Place For Everyone Who Deserves Better</p>
-
-        <Button
-          variant="contained"
-          className="hero-button"
-          onClick={() => navigate("/#services")}>
-          Our Services
-        </Button>
+        <HashLink style={{ textDecoration: "none" }} smooth to="/#services">
+          <Button variant="contained" className="hero-button">
+            Our Services
+          </Button>
+        </HashLink>
         <GetQuote />
       </div>
 
