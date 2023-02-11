21be03e41faf7bfa3a16d83794373ef9b76a6c5f
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-02-11 09:49:32 +0530

    udpates

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,14 +1,14 @@
 export const testimonials = [
   {
-    text: "It has been a great experiance with Source Monks. We are very satisfied with the final out come. We would love to work with them again for our future plans. They are the best at their work",
-    user: "Muddana Yashwanth",
+    text: "sourcemonks makes me more productive and gets the job done in a fraction of the time. I'm glad I found sourcemonks.",
+    user: "Sophia Rocha",
   },
   {
-    text: "We Have reached out to Source Monks to build a swimming pool, the result was very impressive and thanks to Source Monks for making our idea a reality",
-    user: "Ramineni Vikramadhitya",
+    text: "sourcemonks is exactly what I've been looking for.",
+    user: "Asma Hanson",
   },
   {
-    text: "Source Monks knows what they do, We are very impressed by their quality of work. Our sports complex was a master piece. we are very thankfull to you",
-    user: "Yalamanchi Bhargav",
+    text: "With sourcemonks, we have finally accomplished things that have been waiting forever to get done.",
+    user: "Mahir Harrell",
   },
 ];
